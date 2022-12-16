e954d4fdacd55ec496a5c94e3cc67822d0d6da60
zyq <zyq6227738@163.com>
2022-12-16 14:07:54 +0800

    feat: changeWindowCount

--- packages/FlvPlayer/index.tsx
+++ packages/FlvPlayer/index.tsx
@@ -15,6 +15,7 @@ interface Props {
 
 interface FlvPlayerRefHandle {
     addUrl: (addUrl: FlvUrl) => void; // 增加一个或多个视频流
+    changeWindowCount: (count: WindowCount) => void;
 }
 const windowCountTypeMap: Record<WindowCount, any> = {
     1: { r: 1, c: 1 },
@@ -24,6 +25,24 @@ const windowCountTypeMap: Record<WindowCount, any> = {
     9: { r: 3, c: 3 },
 }
 
+/* 
+    因为默认自动播放比较打扰用户，所以还需满足以下条件之一才能自动播放，以实现更好的用户体验
+    视频静音（通过加上 muted 属性）或音量设置为 0
+    用户与网站进行了交互（通过点击、敲击、按键等）
+    站点已被列入浏览器白名单；
+
+    浏览器确定用户频繁使用媒体，自动加入白名单
+    或者通过首选项
+    或其他用户界面功能手动发生
+
+
+    自动播放功能策略授予了 <iframe> 及其文档自动播放支持。
+
+    另外，chromium 内核的浏览器满足以下条件，也可以自动播放。
+
+    用户已将该站点添加到其移动设备的主屏幕
+    或在桌面设备上安装了 PWA。
+*/
 
 const FlvPlayer: ForwardRefRenderFunction<FlvPlayerRefHandle, any> = (props: Props, ref) => {
     const { url, defaultWindowCount = 1, windowCountType, videoRender } = props
@@ -33,14 +52,15 @@ const FlvPlayer: ForwardRefRenderFunction<FlvPlayerRefHandle, any> = (props: Pro
     const gridTemplate = useMemo(() => ({ r: windowCountTypeMap[windowCount]['r'], c: windowCountTypeMap[windowCount]['c']}), [windowCount])
 
     useImperativeHandle(ref, () => ({
-        addUrl: (addUrl) => startPlayVideo(addUrl)
+        addUrl: (addUrl) => startPlayVideo(addUrl),
+        changeWindowCount: (count) => setWindowCount(count),
     }))
     
     // 按窗口个数生成对应数量vide容器
     const generateVideoEl = useCallback(() => {
         const videos: any = []
         for (let i = 0; i < windowCount; i++) {
-             videos.push(<div className='video-box' key={i}><video id={`video_${i}`} className="video-ele" key={i} controls>
+             videos.push(<div className='video-box' key={i}><video id={`video_${i}`} className="video-ele" key={i} controls autoPlay>
                 your bowser
             </video></div>)
         }
@@ -48,6 +68,7 @@ const FlvPlayer: ForwardRefRenderFunction<FlvPlayerRefHandle, any> = (props: Pro
     }, [windowCount])
 
     useEffect(() => {
+        playerIndex.current = 0
         startPlayVideo(url)
     }, [url])
 
@@ -65,15 +86,19 @@ const FlvPlayer: ForwardRefRenderFunction<FlvPlayerRefHandle, any> = (props: Pro
                     // 已经在播放的视频先销毁
                     playerList[playerIndex.current].destroy()
                 }
-                console.log(playerIndex.current)
                 // 根据playerIndex和url实例化播放器
                const playerInstance =  playerList[playerIndex.current] = flvjs.createPlayer({
                     url: item,
-                    type: 'flv'
+                    type: 'flv',
+                }, {
+                    enableWorker: false,
+                    lazyLoadMaxDuration: 3 * 60,
+                    seekType: 'range',
                 })
                 
                 playerInstance.attachMediaElement(videoEls[playerIndex.current]);
                 playerInstance.load();
+
                 playerInstance.play();
                 setPlayerList([...playerList])
                 movePlayerIndex()
@@ -98,7 +123,16 @@ const FlvPlayer: ForwardRefRenderFunction<FlvPlayerRefHandle, any> = (props: Pro
         }}>
             {videoRender?.() ?? generateVideoEl()}
         </div>
-        {windowCountType && <div className="flv-player-count-bar"></div>}
+        {windowCountType && <div className="flv-player-count-bar">
+            <div className="window-count-change-container" style={{
+                display:'grid',
+                gridTemplateColumns: `repeat(${windowCountType.length}, min-content)`,
+                gap: '4px',
+                placeContent: 'center end'
+            }}>
+                {windowCountType.map((item) => <div className='window-count-change-btn' onClick={() => setWindowCount(item)} key={item}>{item}</div>)}     
+            </div>     
+        </div>}
     </div>
 }
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import { FlvPlayer } from '../packages'
 import './App.css'
 import { flv_strs } from './data/flvStr'
 
 function App() {
 
+  const [url, setUrl] = useState<string | string[]>(flv_strs)
   const flvRef = useRef<any>(null)
   const addUrlHandle = () => {
     flvRef?.current?.addUrl?.("wss://videos3.e6yun.com:8080/live/11236408149-1-ems-3bfd1841-acaf-46f9-96be-e9eac138db4a.flv")
+  }
+  const setNewUrl = () => {
+    setUrl(["wss://videos3.e6yun.com:8080/live/11236408149-1-ems-3bfd1841-acaf-46f9-96be-e9eac138db4a.flv"])
   }
   return (
     <div className="App">
         <div style={{ height: 40, padding: 10, background: "rgba(10, 10, 200, .2)" }}>
           <button onClick={addUrlHandle} style={{ padding: "4px 10px"}}>add url</button>
+          <button onClick={setNewUrl} style={{ padding: "4px 10px", marginLeft: 20 }}>use new urls</button>
         </div>
         <div style={{ height: 600 }}>
-        <FlvPlayer ref={flvRef} url={flv_strs} windowCountType={[1,4,6]} defaultWindowCount={4} />
+        <FlvPlayer ref={flvRef} url={url} windowCountType={[1,4,6]} defaultWindowCount={4} />
         </div>
     </div>
   )
